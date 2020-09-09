92f68a0510926c550ced894e6045a15be709d1a3
Parker Johnstone <parker.a.johnstone@gmail.com>
2020-09-09 13:07:42 -0500

    new project creation

---
+++ Assets/Scripts/VirtualButtonHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class VirtualButtonHandler : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject vbGameObject;
+    public GameObject cube;
+    void Start()
+    {
+        vbGameObject.GetComponent<VirtualButtonBehaviour>().RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(false);
+        Debug.Log("vb Pressed");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        cube.SetActive(true);
+        Debug.Log("vb Released");
+    }
+
+}
